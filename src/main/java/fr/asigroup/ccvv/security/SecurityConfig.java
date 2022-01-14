bc1ad510cb561cc1269fcab49ee1b5362cec1206
igor <igor.onouferko@javenir84.com>
2022-01-14 09:21:54 +0100

    add security logic

---
+++ src/main/java/fr/asigroup/ccvv/security/SecurityConfig.java
@@ -0,0 +1,57 @@
+package fr.asigroup.ccvv.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public UserDetailsImplService detailsService() {
+        return new UserDetailsImplService();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(detailsService());
+        provider.setPasswordEncoder(encoder());
+
+        return provider;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/**").authenticated()
+                .anyRequest().permitAll()
+                .and().formLogin()
+                .usernameParameter("name")
+                .defaultSuccessUrl("/").permitAll()
+                .and().logout()
+                .logoutSuccessUrl("/logout").permitAll();
+    }
+}
