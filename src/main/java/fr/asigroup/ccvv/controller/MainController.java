9c2e73a9be843794af44229b109bf1cbc5ed92f5
MadMan <mr.mad2k@gmail.com>
2022-01-24 17:02:43 +0100

    Calendar date generation

--- src/main/java/fr/asigroup/ccvv/controller/MainController.java
+++ src/main/java/fr/asigroup/ccvv/controller/MainController.java
@@ -1,16 +1,16 @@
 package fr.asigroup.ccvv.controller;
 
 
-import fr.asigroup.ccvv.pojo.PasswordsDTO;
-import fr.asigroup.ccvv.service.UserNotFoundException;
+import fr.asigroup.ccvv.pojo.MonthCalendarGenerator;
 import fr.asigroup.ccvv.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Map;
 
 
 @Controller
@@ -22,7 +22,16 @@ public class MainController {
     }
 
     @GetMapping
-    public String showSelection() {
+    public String showSelection(@RequestParam(required = false, defaultValue = "0") Integer offset, Model model) {
+
+        MonthCalendarGenerator monthCalendarGenerator = new MonthCalendarGenerator();
+
+        Map<LocalDate, Map<String, String>> searchedMonthCalendar = monthCalendarGenerator.getMonthCalendar(offset);
+        String searchedMonth = monthCalendarGenerator.getNameOfMonth(offset);
+
+        model.addAttribute("calendar", searchedMonthCalendar);
+        model.addAttribute("month", searchedMonth);
+        model.addAttribute("offset", offset);
 
         return "index";
     }
