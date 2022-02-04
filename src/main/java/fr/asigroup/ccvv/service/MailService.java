63de927fb93063889c676dae53a4b392e6370066
MadMan <mr.mad2k@gmail.com>
2022-02-04 16:53:26 +0100

    add mail code

---
+++ src/main/java/fr/asigroup/ccvv/service/MailService.java
@@ -0,0 +1,24 @@
+package fr.asigroup.ccvv.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailService{
+
+    @Autowired
+    JavaMailSender javaMailSender;
+
+    public void envoiEmail(String destinataire, String sujet, String texte) {
+
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setFrom("philippe.kurz@javenir84.com");  // Expéditeur
+        simpleMailMessage.setTo(destinataire);  // Destinataire
+        simpleMailMessage.setSubject(sujet);
+        simpleMailMessage.setText(texte);
+
+        this.javaMailSender.send(simpleMailMessage);
+    }
+}
